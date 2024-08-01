0639657aaa6cd51e51ecb9dfd1761dc7f94cc3a8
devkyoung2 <devkyoung2@gmail.com>
2024-08-01 17:40:15 +0900

    움직이는 큐브 실습

--- practice1/main.js
+++ practice1/main.js
@@ -1,26 +1,45 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
 
 const renderer = new THREE.WebGLRenderer();
+// 렌더러 사이즈 설정
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const fov = 75;
+const aspect = 2;
+const near = 0.5;
+const far = 5;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+// 카메라 시점
+camera.position.z = 2;
+
+const boxWidth = 1;
+const boxHeight = 1;
+const boxDepth = 1;
+const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+// 지오메트리랑 메트라얼로 메시를 만듬
+const cube = new THREE.Mesh(geometry, material);
+
+// 만들어진 메시를 씬에 넣는다.
+scene.add(cube);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function render(time) {
+  time *= 0.001;
 
-camera.position.z = 5;
+  // 메시를 x, y축 로테이션
+  cube.rotation.x = time;
+  cube.rotation.y = time;
 
-function animate() {
+  // 얘가 화면에 보여지게 하는애임
   renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
 }
-renderer.setAnimationLoop(animate);
+requestAnimationFrame(render);
